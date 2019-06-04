f10501f4b4fad187b7efe39205d8a1248f41a9d3
sofiaalvarezlopez <ms.alvarezl@uniandes.edu.co>
2019-06-04 15:36:05 -0500

    Introduction to Julia

---
+++ Learning Julia.jl
@@ -0,0 +1,26 @@
+
+#Exponents
+a = 9^2
+
+A = [10, 20, 30]
+
+#Julia is 1-indexed
+A[1]
+
+#MATRICES
+
+#Making a 3x3 matrix
+M = [1 2 3;4 5 6;7 8 9]
+
+#In here, I access the position [2,2] of the matrix
+
+M[2,2] = 28
+M
+
+#Here I traspose a matrix
+M'
+
+#And here I calculate the inverse of the matrix
+inv(M)
+
+
