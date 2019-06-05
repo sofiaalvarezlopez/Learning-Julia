3cf73bcaac940b1fa5ae52a6eb0d8fd827533203
sofiaalvarezlopez <ms.alvarezl@uniandes.edu.co>
2019-06-04 20:43:50 -0500

    Data analysis with CSV

--- Learning Julia.jl
+++ Learning Julia.jl
@@ -1,4 +1,7 @@
 
+using DataFrames
+using CSV
+
 #Exponents
 a = 9^2
 
@@ -9,7 +12,7 @@ A[1]
 
 #MATRICES
 
-#Making a 3x3 matrix
+#Hacer una matriz 3x3
 M = [1 2 3;4 5 6;7 8 9]
 
 #In here, I access the position [2,2] of the matrix
@@ -22,5 +25,45 @@ M'
 
 #And here I calculate the inverse of the matrix
 inv(M)
+
+#Dictionaries
+D = Dict("name" => "Sofia Alvarez", "birthyear" => 2000, "university" => "Los Andes")
+
+D["name"]
+
+D.count
+
+#Strings ARE immutable
+nombre = "Sofia Alvarez"
+println(nombre[1])
+nombre[1:5]
+
+#Loops
+var = 0
+stop = 3
+for i=1:stop
+    var = i + pi
+    print(i, " ")
+end
+println("The value of the sum is: ", var)
+
+
+variable = 28
+    if variable == 28
+        println("Tau day!")
+    else
+        println("Ups!")
+end 
+
+personas = CSV.read("MOCK_DATA.csv")
+
+size(personas)
+
+names(personas)
+
+#Summary stats of the column age
+describe(personas[:age])
+
+
 
 
